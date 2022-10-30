cf27f22f13fb6ab6d256c0ec8e3134b24d337397
sarbjit singh <sarbjitsingh@sarbjits-MacBook-Air.local>
2022-10-30 10:49:03 +0000

    font-awesome icons library build

--- App.js
+++ App.js
@@ -4,17 +4,9 @@ import React from 'react';
 // import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faMugSaucer} from '@fortawesome/free-solid-svg-icons/faMugSaucer';
-// in App.js
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {fab} from '@fortawesome/free-brands-svg-icons';
-import {faSquareCheck} from '@fortawesome/free-solid-svg-icons/faSquareCheck';
-
-library.add(fab, faSquareCheck, faMugSaucer);
 
 import WifiManager from 'react-native-wifi-reborn';
-import {faAddressBook, faGear} from '@fortawesome/free-solid-svg-icons';
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {faHourglass} from '@fortawesome/free-regular-svg-icons';
 
 // WifiManager.connectToProtectedSSID(ssid, password, isWep).then(
 //   () => {
@@ -43,7 +35,7 @@ export default function App() {
       <Text>app</Text>
       {/* <Icon name="house" type="font-awesome" size={32} color="#000" /> */}
 
-      <FontAwesomeIcon icon={faEnvelope} size={50} color={'green'} />
+      <FontAwesomeIcon icon={faHourglass} size={50} color={'green'} />
     </View>
   );
 }
